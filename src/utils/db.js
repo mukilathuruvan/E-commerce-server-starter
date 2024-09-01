a330bc78dc22096ab2e4ba7868854ef80185bf5e
mukilathuruvan <mukilathuruvan5241@gmail.com>
2024-09-02 01:35:53 +0530

    db uri changed

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(
+      "mongodb+srv://mukilathuruvan5241:Mukil_5241@cluster0.bqjj0vd.mongodb.net/"
+    );
     console.log("connected to db.");
   } catch (err) {
     console.log(`while connecting db ${err.message}`);
